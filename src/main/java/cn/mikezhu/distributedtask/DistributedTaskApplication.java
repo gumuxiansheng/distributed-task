900691321272f3a6b80e62b1c9e01490a502d77a
mike <zhufuyuan@mikezhu.cn>
2023-04-25 23:22:12 +0800

    Distributed task initial

---
+++ src/main/java/cn/mikezhu/distributedtask/DistributedTaskApplication.java
@@ -0,0 +1,13 @@
+package cn.mikezhu.distributedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTaskApplication.class, args);
+	}
+
+}
