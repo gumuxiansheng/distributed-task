c735429e7897d5fd5179cf9d4c0b8f118c36c1ee
mike <zhufuyuan@mikezhu.cn>
2023-05-10 20:59:53 +0800

    sql语句修改

--- src/main/java/cn/mikezhu/distributedtask/task/Tasks.java
+++ src/main/java/cn/mikezhu/distributedtask/task/Tasks.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 @Component
 public class Tasks {
     @Autowired
@@ -12,6 +15,12 @@ public class Tasks {
 
     @Scheduled(cron = "0/10 * * * * ?")
     public void testTask1() {
-
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        jdbcTemplate.execute(String.format("""
+                UPDATE dev_distributed_task.task
+                SET last_start_time='%s'
+                WHERE task_name='test1';
+                """, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime())));
     }
 }
--- src/main/resources/db/ddl_init_task_table.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists dev_distributed_task.task;
-create table dev_distributed_task.task(
-    task_id serial primary key,
-    task_name varchar(50),
-    last_start_time timestamp,
-    last_end_time timestamp,
-    last_host varchar(20),
-    next_scheduled_time timestamp
-);
